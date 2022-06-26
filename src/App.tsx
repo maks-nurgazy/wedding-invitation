7e50734b4a03acaecc86a7c3f7db29ba4e0f6144
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2022-06-26 17:38:21 +0600

    History page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-/* Local dependencies */
+/* External dependencies */
 
 /* Local dependencies */
 import { Banner } from './components/banner/Banner';
+import { History } from './components/history/History';
 
 function App() {
   return (
     <div className='App'>
       <Banner></Banner>
+      <History></History>
     </div>
   );
 }
