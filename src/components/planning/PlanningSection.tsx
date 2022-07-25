0b56a6618e62987d827438055562e82795de2c8e
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2022-07-25 14:35:27 +0600

    Map size fixed. story text updated

--- src/components/planning/PlanningSection.tsx
+++ src/components/planning/PlanningSection.tsx
@@ -8,20 +8,20 @@ export function PlanningSection() {
         <div className='row clearfix'>
           <div className='col-xl-12 col-lg-12 col-12'>
             <div id='atf-map-area'>
-              <iframe
+              {/* <iframe
                 src='https://www.google.com.qa/maps/d/u/0/embed?mid=1Ig53jTqe4Jr7Vba-yClxy7v8e8ulgLU&ehbc=2E312F'
                 width='640'
                 height='600'
-              ></iframe>
-              {/* <div style={{ width: '100%', overflow: 'hidden', height: '800px' }}>
-              <iframe
-                src='https://www.google.com.qa/maps/d/u/0/embed?mid=1Ig53jTqe4Jr7Vba-yClxy7v8e8ulgLU&ehbc=2E312F'
-                width='1200'
-                height='800'
-                frameBorder={0}
-                style={{ border: 0, marginTop: '-150px' }}
-              ></iframe>
-              </div> */}
+              ></iframe> */}
+              <div style={{ width: '100%', overflow: 'hidden', height: '800px' }}>
+                <iframe
+                  src='https://www.google.com.qa/maps/d/u/0/embed?mid=1Ig53jTqe4Jr7Vba-yClxy7v8e8ulgLU&ehbc=2E312F'
+                  width='1200'
+                  height='800'
+                  frameBorder={0}
+                  style={{ border: 0, marginTop: '-150px' }}
+                ></iframe>
+              </div>
             </div>
           </div>
         </div>
