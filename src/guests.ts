5032a71ffa1cc43e69c19d2f612a811f97d4c54b
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2022-07-25 12:18:19 +0600

    Update guests.ts

--- src/guests.ts
+++ src/guests.ts
@@ -1,4 +1,5 @@
 interface Guest {
+  lang: 'ru' | 'kg';
   title: string;
   name: string;
 }
@@ -7,196 +8,251 @@ type Guests = Record<string, Guest>;
 
 export const guests: Guests = {
   adinai: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Адинай',
   },
   adis: {
+    lang: 'ru',
     title: 'Дорогой',
     name: 'Адис',
   },
   aiana: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Аяна',
   },
   aidai: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айдай',
   },
   aidaibraimova: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Аида Ибраимова',
   },
   aijana: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айжана',
   },
   'aikut-ulkan': {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айкут и Уулкан',
   },
   airin: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айрин',
   },
   aisezim: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айсезим',
   },
   aitemir: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айтемир',
   },
   aizat: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айзат',
   },
   aizirek: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Айзирек',
   },
   aktilek: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Актилек',
   },
   aleksandraleksandrovich: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Александр Александрович',
   },
   alina: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Алина',
   },
   'amantur-aicholpon': {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Амантур жезде и Айчолпон эже',
   },
   annaaleksandrovna: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Анна Александровна',
   },
   arslan: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Арслан',
   },
   azamat: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Азамат',
   },
   begimai: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Бегимай',
   },
   bermet: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Бермет',
   },
   dastan: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Дастан',
   },
   'doston-kulpunai': {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Достон и Кулпунай',
   },
   erlan: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Эрлан',
   },
   elena: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Елана',
   },
   'jeenbek-aigerim': {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Жээнбек и Айгерим',
   },
   gulnara: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Гулнара',
   },
   gulbarchyn: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Гулбарчын',
   },
   'maisalbek-maksuda': {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'папа Майсалбек и мама Максуда',
   },
   maksat: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Максат',
   },
   marat: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Марат',
   },
   'marat-alina': {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Марат и Алина',
   },
   meerimai: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Мээримай',
   },
   mirbek: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Мирбек',
   },
   'nurgazy-ayim': {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Нургазы и Айым',
   },
   nurperi: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Нурпери',
   },
   nursultan: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Нурсултан',
   },
   oscar: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Оскар',
   },
   renat: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Ренат',
   },
   rustam: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Рустам',
   },
   saltanat: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Салтанат',
   },
   shaiken: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Шайкен',
   },
   talant: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Талант',
   },
   tatyana: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Татьяна',
   },
   tynara: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Тынара',
   },
   victoria: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Виктория',
   },
   vlad: {
+    lang: 'ru',
     title: 'Дорогой(ая)',
     name: 'Влад',
+  },
+
+  /* Kyrgyz language */
+  uuljan: {
+    lang: 'kg',
+    title: 'Урматтуу',
+    name: 'Уулжан',
   },
 };
 
 export const defaultGuest: Guest = {
+  lang: 'ru',
   title: 'Дорогой(ая)',
   name: '',
 };
