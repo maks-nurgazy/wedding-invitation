009e4b968a6f96668a7ece541af502d066134816
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2022-07-26 22:45:12 +0600

    light gallery with single image

--- src/components/gallery/LighGallerySection.tsx
+++ src/components/gallery/LighGallerySection.tsx
@@ -27,30 +27,6 @@ export function LightGallerySection() {
           >
             <img className='img-responsive' src='/images/gallery/img-1.jpg' />
           </a>
-          <a
-            data-lg-size='1400-1400'
-            data-pinterest-text='Shinimamiya, Osaka, Japan'
-            data-tweet-text='Shinimamiya, Osaka, Japan'
-            className='gallery-item'
-            data-src='https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'
-            data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Shah </a></h4><p> Location - <a href='https://unsplash.com/s/photos/shinimamiya%2C-osaka%2C-japan'>Shinimamiya, Osaka, Japan</a></p>"
-          >
-            <img
-              className='img-responsive'
-              src='https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80'
-            />
-          </a>
-          <a
-            data-lg-size='1400-1400'
-            className='gallery-item'
-            data-src='https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'
-            data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-          >
-            <img
-              className='img-responsive'
-              src='https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80'
-            />
-          </a>
         </LightGallery>
       </div>
     </section>
